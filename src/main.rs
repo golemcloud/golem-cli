79e370c08707b36f05f2706fbbfe1b1fb9a0de79
Adam Fraser <adam.fraser@gmail.com>
2024-02-08 14:28:41 -0800

    add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Copyright 2024 Golem Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 extern crate derive_more;
 
 use std::fmt::Debug;
@@ -9,12 +23,6 @@ use golem_client::Context;
 use golem_examples::model::{ExampleName, GuestLanguage, GuestLanguageTier, PackageName};
 use reqwest::Url;
 use tracing_subscriber::FmtSubscriber;
-
-use golem_cli::clients::template::TemplateClientLive;
-use golem_cli::clients::worker::WorkerClientLive;
-use golem_cli::examples;
-use golem_cli::template::{TemplateHandler, TemplateHandlerLive, TemplateSubcommand};
-use golem_cli::worker::{WorkerHandler, WorkerHandlerLive, WorkerSubcommand};
 
 #[derive(Subcommand, Debug)]
 #[command()]
