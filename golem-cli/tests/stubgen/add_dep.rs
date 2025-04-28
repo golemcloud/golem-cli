4ec0f234ceb15682e1b7cee5772be8234ff3cc1c
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2025-04-28 14:04:54 +0200

    Turn off keeping temp dirs

--- golem-cli/tests/stubgen/add_dep.rs
+++ golem-cli/tests/stubgen/add_dep.rs
@@ -556,7 +556,7 @@ fn self_circular() {
 }
 
 fn init_stub(name: &str) -> (TempDir, TempDir) {
-    let source = tempfile::Builder::new().keep(true).tempdir().unwrap();
+    let source = tempfile::Builder::new().keep(false).tempdir().unwrap();
     let canonical_source = source.path().canonicalize().unwrap();
 
     fs_extra::dir::copy(
@@ -566,7 +566,7 @@ fn init_stub(name: &str) -> (TempDir, TempDir) {
     )
     .unwrap();
 
-    let target = tempfile::Builder::new().keep(true).tempdir().unwrap();
+    let target = tempfile::Builder::new().keep(false).tempdir().unwrap();
     let canonical_target = target.path().canonicalize().unwrap();
 
     let def = StubDefinition::new(StubConfig {
@@ -602,7 +602,7 @@ fn regenerate_stub(stub_dir: &Path, source_wit_root: &Path) {
 }
 
 fn init_caller(name: &str) -> TempDir {
-    let temp_dir = tempfile::Builder::new().keep(true).tempdir().unwrap();
+    let temp_dir = tempfile::Builder::new().keep(false).tempdir().unwrap();
     let source = Path::new("test-data/wit").join(name);
 
     fs_extra::dir::copy(
